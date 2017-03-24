b09aa4ac7d0243154828a9d6f9f97d2d18545d70
Nicolas Palermo <nico.palermo@gmail.com>
2017-03-24 12:53:15 -0300

    Vista de mapas completada
    archivos crerados:
    maps.css
    hideshow.js
    
    Se completo la vista de mapas
    El archivo hideshow.js contiene las funciones necesarias para ocultar los mapas cuando se aprietan los botones.
    El archivo maps.css define las clases css de la vista de mapas

---
+++ mapas/hideshow.js
@@ -0,0 +1,26 @@
+﻿jQuery(document).ready(function () {
+   
+    jQuery('#hideshowCuenca').on('click', function (event) {
+        jQuery('#mapacuenca').show();
+        jQuery('#maparegion').hide();
+        jQuery('#mapasubregion').hide();
+    });
+
+    jQuery('#hideshowRegion').on('click', function (event) {
+        jQuery('#maparegion').show();
+        jQuery('#mapasubregion').hide();
+        jQuery('#mapacuenca').hide();
+    });
+
+    jQuery('#hideshowSubregion').on('click', function (event) {
+        console.log('tuvieja');
+        jQuery('#mapasubregion').show();
+        jQuery('#maparegion').hide();
+        jQuery('#mapacuenca').hide();
+    });
+    document.getElementById('iFrameMapaCuenca').onload = function () {
+        jQuery('#mapacuenca').show();
+        jQuery('#maparegion').hide();
+        jQuery('#mapasubregion').hide();
+    };
+});
